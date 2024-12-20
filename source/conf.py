ac39ad56f2ece837bedef471582d591dd1a67f06
adelaunay <delaunay.alexandre@gmail.com>
2024-12-20 11:51:33 +0100

    try immaterial theme

--- source/conf.py
+++ source/conf.py
@@ -36,6 +36,7 @@ import sphinx_glpi_theme
 extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.ifconfig',
+    'sphinx_immaterial',
 ]
 
 todo_include_todos = True
@@ -132,7 +133,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'glpi'
+html_theme = 'sphinx_immaterial'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
